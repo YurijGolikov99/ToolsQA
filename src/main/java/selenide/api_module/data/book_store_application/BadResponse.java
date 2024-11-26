af99d633935e7be9277994eb4fc2a19a433231a6
golikovyurijalexandrovich <yurij.golikov99@mail.ru>
2024-11-26 16:00:19 +0300

    Refactoring project structure
     1.add gitignore settings;
     2.add comments for pojo class;
     3.replace junit annotation test by testng in selenium tests for;
     4.Change usernames in according dependencies;
     5.modify pomxml;
     6.

--- src/main/java/selenide/api_module/data/book_store_application/BadResponse.java
+++ src/main/java/selenide/api_module/data/book_store_application/BadResponse.java
@@ -1,6 +1,8 @@
 package selenide.api_module.data.book_store_application;
 
 /**
+ * DTO
+ * или так называемые POJO class
  * {
  *   "code": 0,
  *   "message": "string"
--- src/main/java/selenium/common_module/data/Credentials.java
+++ src/main/java/selenium/common_module/data/Credentials.java
@@ -1,7 +1,7 @@
 package selenium.common_module.data;
 
 public class Credentials {
-    public static String TEST_USERNAME  = "YurecAQA";
+    public static String TEST_USERNAME  = "YurecTestNG";
     public static String TEST_PASSWORD = "Yurij.Golikov_1999*";
     public static String WRONG_LOGIN = "wrong";
     public static String WRONG_PASSWORD = "wrong";
